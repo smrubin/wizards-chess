21a64ec15940fd09b832a12fc4489ae7696e6a19
smrubin <sam@storyblocks.com>
2018-06-23 13:08:00 -0400

    Updated board initial config

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
 (function() {
 
-    var board1 = ChessBoard('board1', 'start');
+    var initPosition = {
+        f8: 'bR', // Hermione
+        h8: 'bK',
+        b7: 'bP',
+        c7: 'wN',
+        e7: 'wR',
+        g7: 'bP',
+        h7: 'bP',
+        b6: 'wP',
+        c6: 'bB',
+        a5: 'bN',
+        e5: 'wP',
+        g5: 'bN', // Ron
+        h4: 'wN',
+        a3: 'bB', // Harry
+        c3: 'wQ',
+        h2: 'wP',
+        b1: 'wR',
+        g1: 'wK'
+    };
+
+    var onDragStart = function() {
+        $('.error').html('This is Wizard\'s Chess');
+        return false;
+    };
+
+    var wizardBoard = ChessBoard('wizard-board', {
+        position: initPosition,
+        draggable: true,
+        onDragStart: onDragStart
+    });
+
+    // setup new event for voice activation and trigger piece moving in correct
+
+    // hook up erorr and success responses, with sound and then final page
 
 })();
